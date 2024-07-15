591f55b6b23ec77009ec3b6a5ed3e128ca05bea0
TuxoYKT <TuxoYKT@users.noreply.github.com>
2024-07-15 16:37:42 +0900

    Delete config/scripts/fix-mediatek-bt.sh
    fixed in newer kernel

--- config/scripts/fix-mediatek-bt.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-ln -s /lib/firmware/mediatek/BT_RAM_CODE_MT7961_1_2_hdr.bin.xz /lib/firmware/mediatek/BT_RAM_CODE_MT7961_1a_2_hdr.bin.xz
